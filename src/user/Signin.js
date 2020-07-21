10a711954553f624ffd63cdde5b7894aef1e844a
Jyotiprakash Sethy <3561167-Jyotiprakash1@users.noreply.gitlab.com>
2020-07-21 15:13:02 +0530

    Modified stuck

--- src/user/Signin.js
+++ src/user/Signin.js
@@ -1,14 +1,124 @@
-import React, { useState } from 'react';
-import Base from '../core/Base';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import Base from "../core/Base";
+import { Link, Redirect } from "react-router-dom";
 
+import { signin, authenticate, isAuthenticated } from "../auth/helper";
 
 const Signin = () => {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+    error: "",
+    loading: false,
+    didRedirect: false,
+  });
+
+  const { email, password, error, loading, didRedirect } = values;
+  const { user } = isAuthenticated();
+
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, error: false, [name]: event.target.value });
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setValues({ ...values, error: false, loading: true });
+    signin({ email, password })
+      .then((data) => {
+        if (data.error) {
+          setValues({ ...values, error: data.error, loading: false });
+        } else {
+          authenticate(data, () => {
+            setValues({
+              ...values,
+              didRedirect: true,
+            });
+          });
+        }
+      })
+      .catch(console.log("Signin Request Failed"));
+    // .alert("Signin Request Failed");
+  };
+
+  const performRedirect = () => {
+    if (didRedirect) {
+      if (user && user.role === 1) {
+        return <p>redirect to admin</p>;
+      } else {
+        return <p>redirect to user dashboard</p>;
+      }
+    }
+    if (isAuthenticated()) {
+      return <Redirect to="/" />;
+    }
+  };
+
+  const signinForm = () => {
     return (
-        <Base title="Signin up Page" description="A Page For User Signup!">
-            <h1>Signin works</h1>
-        </Base>
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <form>
+            <div className="form-group">
+              <label className="text-light">Email</label>
+              <input
+                onChange={handleChange("email")}
+                value={email}
+                className="form-control"
+                type="email"
+              />
+            </div>
+            <div className="form-group">
+              <label className="text-light">Password</label>
+              <input
+                onChange={handleChange("password")}
+                value={password}
+                className="form-control"
+                type="password"
+              />
+            </div>
+            <button onClick={onSubmit} className="btn btn-success btn-block">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
     );
+  };
+
+  const loadingMessage = () => {
+    return (
+      loading && (
+        <div class="alert alert-info">
+          <h2>Loading...</h2>
+        </div>
+      )
+    );
+  };
+
+  const errorMessage = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-sm-3">
+          <div
+            className="alert alert-danger"
+            style={{ display: error ? "" : "none" }}
+          >
+            {error}
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <Base title="Signin Page" description="A Page For User Signup!">
+      {loadingMessage()}
+      {errorMessage()}
+      {signinForm()}
+      {performRedirect()}
+      <p className="text-white text-white">{JSON.stringify.values}</p>
+    </Base>
+  );
 };
 
 export default Signin;
--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,39 +1,113 @@
-import React, { useState } from 'react';
-import Base from '../core/Base';
-import { Link } from 'react-router-dom';
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Base from "../core/Base";
+import { signup } from "./helper";
 
 const Signup = () => {
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+    error: "",
+    success: false,
+  });
 
-    const signupForm = () => {
-        return (
-            <div className="row">
-                <div className="col-md-6 offset-sm-3 text-left">
-                    <form>
-                        <div className="form-group">
-                            <label className="text-light">Name</label>
-                            <input type="text" />
-                        </div>
-                        <div className="form-group">
-                            <label className="text-light">Email</label>
-                            <input type="email" />
-                        </div>
-                        <div className="form-group">
-                            <label className="text-light">Password</label>
-                            <input type="password" />
-                        </div>
-                        <button className="btn btn-success btn-block">Submit</button>
-                    </form>
-                </div>
-            </div>
-        )
-    }
+  const { name, email, password, error, success } = values;
+
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, error: false, [name]: event.target.value });
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setValues({ ...values, error: false });
+    signup({ name, email, password })
+      .then((data) => {
+        if (data.error) {
+          setValues({ ...values, error: data.error, success: false });
+        } else {
+          setValues({
+            ...values,
+            name: "",
+            email: "",
+            password: "",
+            error: "",
+            success: true,
+          });
+        }
+      })
+      .catch(console.log("error in signup"));
+  };
+
+  const signupForm = () => {
     return (
-        <Base title="Sign up Page" description="A Page For User Signup!">
-            <h1>Signup works</h1>
-            {signupForm()}
-        </Base>
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <form>
+            <div className="form-group">
+              <label className="text-light">Name</label>
+              <input
+                className="form-control"
+                onChange={handleChange("name")}
+                type="text"
+              />
+            </div>
+            <div className="form-group">
+              <label className="text-light">Email</label>
+              <input
+                className="form-control"
+                onChange={handleChange("email")}
+                type="email"
+              />
+            </div>
+            <div className="form-group">
+              <label className="text-light">Password</label>
+              <input
+                className="form-control"
+                onChange={handleChange("password")}
+                type="password"
+              />
+            </div>
+            <button onClick={onSubmit} className="btn btn-success btn-block">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
     );
+  };
+
+  const successMessage = () => {
+    return (
+      <div
+        className="alert alert-success"
+        style={{ display: success ? "" : "none" }}
+      >
+        New Account has been Created Successfully. Please{" "}
+        <Link to="/signin">Login Here</Link>
+      </div>
+    );
+  };
+
+  const errorMessage = () => {
+    return (
+      <div
+        className="alert alert-danger"
+        style={{ display: error ? "" : "none" }}
+      >
+        {error}
+      </div>
+    );
+  };
+
+  return (
+    <Base title="Sign up Page" description="A Page For User Signup!">
+      {successMessage()}
+      {errorMessage()}
+      {signupForm()}
+      <p className="text-white text-center">{JSON.stringify(values)}</p>
+    </Base>
+  );
 };
 
 export default Signup;
