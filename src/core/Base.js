b644c8456c5d8794c20cb6bb71c379848db4ccb1
Jyotiprakash Sethy <3561167-Jyotiprakash1@users.noreply.gitlab.com>
2021-01-26 18:06:21 +0530

    Admin UI

--- src/core/Base.js
+++ src/core/Base.js
@@ -1,68 +1,33 @@
 import React from "react";
 import Menu from "./Menu";
 
-// const Base = ({
-//   title = "My Title",
-//   description = "My desription",
-//   className = "bg-dark text-white p-4",
-//   children
-// }) => (
-//   <div>
-//     <Menu />
-//     <div className="container-fluid">
-//       <div className="jumbotron bg-dark text-white text-center">
-//         <h2 className="display-6">{title}</h2>
-//         <p className="lead">{description}</p>
-//       </div>
-//       <div className={className}>{children}</div>
-//     </div>
-//     <footer className="footer bg-dark mt-auto py-6">
-//       <div className="container-fluid bg-success text-white text-center p-9">
-//         <h4>If you got any questions, feel free to reach out!</h4>
-//         <button className="btn btn-warning btn-lg">Contact Us</button>
-//       </div>
-//       <div className="container">
-//         <span className="text-muted">
-//           An Amazing <span className="text-white">MERN</span> Bootcamp
-//         </span>
-//       </div>
-//     </footer>
-//   </div>
-// );
-
-// export default Base;
-
-// import React from 'react';
-// import Menu from './Menu';
-
-
 const Base = ({
-    title = "My Tittle",
-    description = "My Description",
-    className = "bg-dark text-white p-4",
-    children
+  title = "My Tittle",
+  description = "My Description",
+  className = "bg-dark text-white p-4",
+  children,
 }) => (
-        <div>
-            <Menu />
-            <div className="container-fluid">
-                <div className="jumbotron bg-dark text-white text-center">
-                    <h2 className="display-4">{title}</h2>
-                    <p className="lead">{description}</p>
-                </div>
-                <div className={className}>{children}</div>
-            </div>
-            <footer className="footer bg-dark mt-auto pu-3">
-                <div className="container-fluid bg-success text-white text-center p-3">
-                    <h4>If You Got Any Questions, Feel free to reach out</h4>
-                    <button className="btn btn-warning btn-lg">Contact US</button>
-                </div>
-                <div className="container">
-                    <span className="text-muted">
-                        An Amazing <span className="text-white">MERN</span> Bootcamp
-                </span>
-                </div>
-            </footer>
-        </div>
-    );
+  <div>
+    <Menu />
+    <div className='container-fluid'>
+      <div className='jumbotron bg-dark text-white text-center'>
+        <h2 className='display-6'>{title}</h2>
+        <p className='lead'>{description}</p>
+      </div>
+      <div className={className}>{children}</div>
+    </div>
+    {/* <footer className='footer bg-dark mt-auto py-9'>
+      <div className='container-fluid bg-success text-white text-center py-3'>
+        <h4>If You Got Any Questions, Feel free to reach out</h4>
+        <button className='btn btn-warning btn-lg'>Contact US</button>
+      </div>
+      <div className='container'>
+        <span className='text-muted'>
+          An Amazing <span className='text-white'>MERN</span> Bootcamp
+        </span>
+      </div>
+    </footer> */}
+  </div>
+);
 
 export default Base;
--- src/user/Signin.js
+++ src/user/Signin.js
@@ -1,8 +1,133 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
+// import Base from "../core/Base";
+// import { Link, Redirect } from "react-router-dom";
+
+// import { signin, authenticate, isAutheticated } from "../auth/helper";
+
+// const Signin = () => {
+//   const [values, setValues] = useState({
+//     email: "",
+//     password: "",
+//     error: "",
+//     loading: false,
+//     didRedirect: false
+//   });
+
+//   const { email, password, error, loading, didRedirect } = values;
+//   const { user } = isAutheticated();
+
+//   const handleChange = name => event => {
+//     setValues({ ...values, error: false, [name]: event.target.value });
+//   };
+
+//   const onSubmit = event => {
+//     event.preventDefault();
+//     setValues({ ...values, error: false, loading: true });
+//     signin({ email, password })
+//       .then(data => {
+//         if (data.error) {
+//           setValues({ ...values, error: data.error, loading: false });
+//         } else {
+//           authenticate(data, () => {
+//             setValues({
+//               ...values,
+//               didRedirect: true
+//             });
+//           });
+//         }
+//       })
+//       .catch(console.log("signin request failed"));
+//   };
+
+//   const performRedirect = () => {
+//     if (didRedirect) {
+//       if (user && user.role === 1) {
+//         return <p>redirect to admin</p>;
+//       } else {
+//         return <p>redirect to user dashboard</p>;
+//       }
+//     }
+//     if (isAutheticated()) {
+//       return <Redirect to="/" />;
+//     }
+//   };
+
+//   const loadingMessage = () => {
+//     return (
+//       loading && (
+//         <div className="alert alert-info">
+//           <h2>Loading...</h2>
+//         </div>
+//       )
+//     );
+//   };
+
+//   const errorMessage = () => {
+//     return (
+//       <div className="row">
+//         <div className="col-md-6 offset-sm-3 text-left">
+//           <div
+//             className="alert alert-danger"
+//             style={{ display: error ? "" : "none" }}
+//           >
+//             {error}
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   };
+
+//   const signInForm = () => {
+//     return (
+//       <div className="row">
+//         <div className="col-md-6 offset-sm-3 text-left">
+//           <form>
+//             <div className="form-group">
+//               <label className="text-light">Email</label>
+//               <input
+//                 onChange={handleChange("email")}
+//                 value={email}
+//                 className="form-control"
+//                 type="email"
+//               />
+//             </div>
+
+//             <div className="form-group">
+//               <label className="text-light">Password</label>
+//               <input
+//                 onChange={handleChange("password")}
+//                 value={password}
+//                 className="form-control"
+//                 type="password"
+//               />
+//             </div>
+//             <button onClick={onSubmit} className="btn btn-success btn-block">
+//               Submit
+//             </button>
+//           </form>
+//         </div>
+//       </div>
+//     );
+//   };
+
+//   return (
+//     <Base title="Sign In page" description="A page for user to sign in!">
+//       {loadingMessage()}
+//       {errorMessage()}
+//       {signInForm()}
+//       {performRedirect()}
+//       <p className="text-white text-center">{JSON.stringify(values)}</p>
+//     </Base>
+//   );
+// };
+
+// export default Signin;
+
+import React, { useState, Fragment} from "react";
 import Base from "../core/Base";
 import { Link, Redirect } from "react-router-dom";
 
-import { signin, authenticate, isAutheticated } from "../auth/helper";
+import { signin, authenticate, isAuthenticated } from "../auth/helper";
 
 const Signin = () => {
   const [values, setValues] = useState({
@@ -10,74 +135,52 @@ const Signin = () => {
     password: "",
     error: "",
     loading: false,
-    didRedirect: false
+    didRedirect: false,
   });
 
   const { email, password, error, loading, didRedirect } = values;
-  const { user } = isAutheticated();
+  const { user } = isAuthenticated();
 
-  const handleChange = name => event => {
+  const handleChange = (name) => (event) => {
     setValues({ ...values, error: false, [name]: event.target.value });
   };
 
-  const onSubmit = event => {
+  const onSubmit = (event) => {
     event.preventDefault();
     setValues({ ...values, error: false, loading: true });
     signin({ email, password })
-      .then(data => {
+      .then((data) => {
         if (data.error) {
           setValues({ ...values, error: data.error, loading: false });
         } else {
           authenticate(data, () => {
             setValues({
               ...values,
-              didRedirect: true
+              didRedirect: true,
             });
           });
         }
       })
-      .catch(console.log("signin request failed"));
+      .catch(console.log("Signin Request Failed"));
+    // .console.log("Signin Request Failed");
   };
 
   const performRedirect = () => {
+    // TODO: Do a redirection here
     if (didRedirect) {
       if (user && user.role === 1) {
-        return <p>redirect to admin</p>;
+        return <Redirect to="/admin/dashboard" />;
       } else {
-        return <p>redirect to user dashboard</p>;
+        return <Redirect to="/user/dashboard" />;
       }
     }
-    if (isAutheticated()) {
+    if (isAuthenticated()) {
       return <Redirect to="/" />;
+      // return <p>user Dash</p>;
     }
   };
 
-  const loadingMessage = () => {
-    return (
-      loading && (
-        <div className="alert alert-info">
-          <h2>Loading...</h2>
-        </div>
-      )
-    );
-  };
-
-  const errorMessage = () => {
-    return (
-      <div className="row">
-        <div className="col-md-6 offset-sm-3 text-left">
-          <div
-            className="alert alert-danger"
-            style={{ display: error ? "" : "none" }}
-          >
-            {error}
-          </div>
-        </div>
-      </div>
-    );
-  };
-
-  const signInForm = () => {
+  const signinForm = () => {
     return (
       <div className="row">
         <div className="col-md-6 offset-sm-3 text-left">
@@ -91,7 +194,6 @@ const Signin = () => {
                 type="email"
               />
             </div>
-
             <div className="form-group">
               <label className="text-light">Password</label>
               <input
@@ -110,13 +212,38 @@ const Signin = () => {
     );
   };
 
+  const loadingMessage = () => {
+    return (
+      loading && (
+        <div class="alert alert-info">
+          <h2>Loading...</h2>
+        </div>
+      )
+    );
+  };
+
+  const errorMessage = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-sm-3">
+          <div
+            className="alert alert-danger"
+            style={{ display: error ? "" : "none" }}
+          >
+            {error}
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   return (
-    <Base title="Sign In page" description="A page for user to sign in!">
+    <Base title="Signin Page" description="A Page For User Signup!">
       {loadingMessage()}
       {errorMessage()}
-      {signInForm()}
+      {signinForm()}
       {performRedirect()}
-      <p className="text-white text-center">{JSON.stringify(values)}</p>
+      <p className="text-white text-white">{JSON.stringify.values}</p>
     </Base>
   );
 };
